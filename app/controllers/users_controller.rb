b904a200e244ebb16b76cc54bdd77e65a1590e9e
brad morgan <bkmorgan3@gmail.com>
2017-06-26 16:50:16 -0700

    new user form works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,41 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(set_user)
+    if @user.save
+      flash[:notice] = "Sign up was succesful."
+      redirect_to users_path
+    else
+      redirect_to new_user_path
+  end
+end
+
+  def update
+  end
+
+
+  def destroy
+  end
+
+  private
+  def set_user
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/users' => 'users#index'
+  get '/users' => 'users#index', as: :users
+
+  get 'users/new' => 'users#new', as: :new_user
+
+  get 'users/:id' => 'users#show', as: :user
+
+  post 'users' => 'users#create'
 end
