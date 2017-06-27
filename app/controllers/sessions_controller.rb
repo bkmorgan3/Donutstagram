5cbabb304c50ed89beb42a994193f3fd1c093f73
brad morgan <bkmorgan3@gmail.com>
2017-06-26 18:03:38 -0700

    login to users own page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,23 @@ class SessionsController < ApplicationController
   end
 
   def create
-    #first try and find the user by username in the db
-    #@user = User.find_by_username([:login][:username])
+    #first try and find the user by email in the db
+    @user = User.find_by_email(params[:login][:email])
     #if the user exists AND they put in the right password:
-    #if @user && @user.authenticate
+    if @user && @user.authenticate(params[:login][:password])
+      #generate a cookie for the user
+      #redirect them somewhere
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      #redirect them back to the login page
+      redirect_to login_path
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to login_path, notice: "Logged out!"
   end
 
 end
